14592dd868fd94264c5272087403499b930808a0
Vicky-2000 <56164489+Vicky-2000@users.noreply.github.com>
2019-10-05 00:07:15 +0530

    Update extracting.c

--- extracting.c
+++ extracting.c
@@ -2,12 +2,17 @@
 int main()
 {int i,j,c,b;
 char a[90],temp[90];
+ printf("enter any word");
 gets(a);
-scanf("%d%d",&c,&b);
+ printf("extracting origin");
+ scanf("%d",&c);
+ printf("extracting end");
+ scanf("%d",&b);
 for(i=c;i<=b;i++)
 {temp[j]=a[i];
 j++;
 }
+ printf("the extracted word");
 temp[j]='\0';
 puts(temp);
 }
